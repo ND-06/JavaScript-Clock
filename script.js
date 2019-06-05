2a7d969e1dda261c214110db1f89dcbd34b8c1dd
Natdenice <clad1987@hotmail.com>
2019-06-05 14:08:51 +0200

    width and height of hands modified

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-unused-vars */
 // First thing we need to do is Create a function called set Date
 
-const secondHand = document.querySelector('.second-hand');
+/*
+const secondHand = document.querySelector('.sec-hand');
 const minuteHand = document.querySelector('.min-hand');
 const hourHand = document.querySelector('.hour-hand');
 
@@ -14,7 +16,27 @@ const setDate = () => {
   const hoursDegrees = (hours / 12) * 360 + 90;
   secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
   minuteHand.style.transform = `rotate(${minutesDegrees}deg)`;
-  hourHand.style.transform = `rotate(${hoursDegrees}deg)`;
+  hourHand.style.transform = `rotate(${hoursDegrees}deg) scale(0.5,1)`;
+};
+
+setInterval(setDate, 1000);
+*/
+
+const secondHand = document.querySelector('.sec-hand');
+const minutesHand = document.querySelector('.min-hand');
+const hoursHand = document.querySelector('.hour-hand');
+
+const setDate = () => {
+  const now = new Date();
+  const seconds = now.getSeconds();
+  const minutes = now.getMinutes();
+  const hours = now.getHours();
+  const secondsDegrees = (seconds / 60) * 360 + 90;
+  const minutesDegrees = (minutes / 60) * 360 + 90;
+  const hoursDegrees = (hours / 12) * 360 + 90;
+  secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+  minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+  hoursHand.style.transform = `rotate(${hoursDegrees}deg) scale(0.5,1)`;
 };
 
 setInterval(setDate, 1000);
